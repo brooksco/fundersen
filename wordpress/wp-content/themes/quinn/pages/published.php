77e0573f39b2d2be0a41b4498fcbaecaa321e6d3
Colin Brooks <colinbooks@gmail.com>
2013-10-05 00:53:53 +1300

    published page, reel

--- wordpress/wp-content/themes/quinn/pages/published.php
+++ wordpress/wp-content/themes/quinn/pages/published.php
@@ -1,2 +1,21 @@
+<div class="published">
+
+<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/prague-book-cover.jpg" />
+
+	<h3>World Film Locations: Prague</h3>
+	<h4>Photography contributor</h4>
+
+	<p style="">
+
+		Irony DIY roof party, pop-up XOXO fashion axe selvage blog PBR&B
+		literally you probably haven't heard of them. Single-origin coffee
+		blog forage put a bird on it. Small batch keytar asymmetrical
+		Williamsburg locavore leggings disrupt wayfarers. Irony pork belly
+		forage readymade polaroid Blue Bottle, cardigan whatever fanny pack
+		wolf Austin distillery locavore vinyl.
+
+	</p>
 
 <a href="http://www.amazon.com/World-Film-Locations-Prague-Intellect/dp/1783200278/">Amazon</a>
+
+</div>
---
+++ wordpress/wp-content/themes/quinn/pages/reel.php
@@ -0,0 +1,6 @@
+
+<div class="responsive-container">
+<iframe src="//player.vimeo.com/video/10924639?title=0&amp;byline=0&amp;portra
+<it=0&amp;color=65d992" frameborder="0"
+<webkitallowfullscreen mozallowfullscreen allowfullscreen>/iframe>
+</div>
